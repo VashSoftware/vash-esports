ac4c428d943d6934d7609c3904112de6b41fa2dd
Stan Runge <stan@stanrunge.dev>
2024-01-31 22:41:17 +0100

    Add Vercel adapter for deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,9 @@
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "@sveltejs/adapter-vercel";
 
 export default {
   preprocess: [vitePreprocess({})],
+  kit: {
+    adapter: adapter(),
+  },
 };
