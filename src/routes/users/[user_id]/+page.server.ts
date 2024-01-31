8c2e5e8a0a4646297ffec5029cc3d72ff005d226
Stan Runge <stan@stanrunge.dev>
2024-01-31 22:50:12 +0100

    Update user query to use user_profiles table

--- src/routes/users/[user_id]/+page.server.ts
+++ src/routes/users/[user_id]/+page.server.ts
@@ -2,7 +2,7 @@ import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ locals }) => {
   const { data, error } = await locals.supabase
-    .from("users")
+    .from("user_profiles")
     .select("*, organisation_members(*)")
     .eq("id", 1)
     .single();
