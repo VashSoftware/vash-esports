46f885f9e9930a41f66d4698eabcac4756ff8c6c
Stan Runge <stan@stanrunge.dev>
2024-02-02 03:36:13 +0100

    style: update global search bar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,8 +12,24 @@
         <a class="navbar-brand fs-3" href="/"><b>Vash Esports</b></a>
       </div>
       <div class="col text-center">
-        <form method="post">
-          <input type="search" class="form-control" placeholder="Search"/>
+        <form method="post" class="d-flex">
+          <div class="input-group">
+            <input type="search" class="form-control" placeholder="Search" />
+            <button type="submit" class="btn btn-secondary"
+              ><svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="1.5em"
+                height="1.5em"
+                viewBox="0 0 24 24"
+                {...$$props}
+              >
+                <path
+                  fill="currentColor"
+                  d="m19.6 21l-6.3-6.3q-.75.6-1.725.95T9.5 16q-2.725 0-4.612-1.888T3 9.5q0-2.725 1.888-4.612T9.5 3q2.725 0 4.613 1.888T16 9.5q0 1.1-.35 2.075T14.7 13.3l6.3 6.3zM9.5 14q1.875 0 3.188-1.312T14 9.5q0-1.875-1.312-3.187T9.5 5Q7.625 5 6.313 6.313T5 9.5q0 1.875 1.313 3.188T9.5 14"
+                />
+              </svg></button
+            >
+          </div>
         </form>
       </div>
       <div class="col p-0">
