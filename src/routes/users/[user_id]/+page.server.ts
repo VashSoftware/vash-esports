7f3498fb627aab8b9f9246b47d1d12697da2b4e8
Stan Runge <stan@stanrunge.dev>
2024-02-04 23:04:59 +0100

    feat: add organisation icons to user page

--- src/routes/users/[user_id]/+page.server.ts
+++ src/routes/users/[user_id]/+page.server.ts
@@ -1,18 +1,27 @@
 import type { PageServerLoad } from "./$types";
 
-export const load: PageServerLoad = async ({ locals }) => {
+export const load: PageServerLoad = async ({ locals, params }) => {
   const { data, error } = await locals.supabase
     .from("user_profiles")
-    .select("*, organisation_members(*)")
-    .eq("id", 1)
+    .select("*, organisation_members(*), team_members(*)")
+    .eq("id", params.user_id)
     .single();
 
   const userPictureUrl = await locals.supabase.storage
     .from("user_pictures")
-    .getPublicUrl(locals.user?.id);
+    .getPublicUrl(params.user_id);
+
+  let organisationPublicUrls = [];
+  for (let i = 0; i < data?.organisation_members.length; i++) {
+    const organisationPublicUrl = await locals.supabase.storage
+      .from("organisation_logos")
+      .getPublicUrl(data.organisation_members[i].organisation_id);
+    organisationPublicUrls.push(organisationPublicUrl);
+  }
 
   return {
     user: data,
     userPictureUrl: userPictureUrl.data.publicUrl,
+    organisationPublicUrls: organisationPublicUrls,
   };
 };
