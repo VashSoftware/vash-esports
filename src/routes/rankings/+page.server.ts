410f0f8c760e4d79e4e4458fb1a1424019d428a2
Stan Runge <stan@stanrunge.dev>
2024-02-05 02:34:37 +0100

    perf: fetch data in a Promise.all

--- src/routes/rankings/+page.server.ts
+++ src/routes/rankings/+page.server.ts
@@ -1,19 +1,30 @@
 export async function load(request) {
-  const userRatings = await request.locals.supabase
+  const userRatingsPromise = request.locals.supabase
     .from("user_ratings")
     .select(
       "*, user_profiles (name, team_members (teams( participants (match_participants (matches (count))))))"
     );
 
-  const matchesPlayed = await request.locals.supabase
+  const matchesPlayedPromise = request.locals.supabase
     .from("matches")
     .select(
       `*, rounds (name, events (id, name)), match_participants (participants (teams (name)))`
     );
 
-  const teams = await request.locals.supabase.from("teams").select("*, participants(count)");
+  const teamsPromise = request.locals.supabase
+    .from("teams")
+    .select("*, participants(count)");
 
-  const organisations = await request.locals.supabase.from('organisations').select('*, events(count)')
+  const organisationsPromise = request.locals.supabase
+    .from("organisations")
+    .select("*, events(count)");
+
+  const [userRatings, matchesPlayed, teams, organisations] = await Promise.all([
+    userRatingsPromise,
+    matchesPlayedPromise,
+    teamsPromise,
+    organisationsPromise,
+  ]);
 
   return {
     userRatings: userRatings.data,
