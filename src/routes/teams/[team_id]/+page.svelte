5398b926a253c15e5e075b2528682b88110e4c14
Stan Runge <stan@stanrunge.dev>
2024-02-16 02:46:42 +0100

    seo: add metadata to team page

--- src/routes/teams/[team_id]/+page.svelte
+++ src/routes/teams/[team_id]/+page.svelte
@@ -1,16 +1,35 @@
 <script lang="ts">
+  import { page } from "$app/stores";
+
   export let data;
 </script>
 
+<svelte:head>
+  <title>{data.team.name} | Vash Esports</title>
+  <meta name="description" content={data.team.bio} />
+  <meta name="og:url" content={$page.url.href} />
+  <meta name="og:title" content={data.team.name} />
+  <meta name="og:description" content={data.team.bio} />
+  <meta name="og:image" content={data.teamPicture.data.publicUrl} />
+
+  <meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
+
 <div class="py-5 row">
   <div class="col d-flex align-items-center">
-    <img src={data.teamPicture.data.publicUrl} height="128" alt="Team icon" class="rounded me-4">
+    <img
+      src={data.teamPicture.data.publicUrl}
+      height="128"
+      alt="Team icon"
+      class="rounded me-4"
+    />
     <h1>{data.team.name}</h1>
   </div>
 
   <div class="col text-end d-flex justify-content-end align-items-center">
     <form method="post">
-      <button class="btn btn-primary mx-3" type="submit">Request to Join</button>
+      <button class="btn btn-primary mx-3" type="submit">Request to Join</button
+      >
     </form>
 
     <a
@@ -24,7 +43,11 @@
 <h2>Team Members</h2>
 <ul>
   {#each data.team.team_members as teamMember}
-    <li><a href="/users/{teamMember.user_profiles.id}">{teamMember.user_profiles.name}</a></li>
+    <li>
+      <a href="/users/{teamMember.user_profiles.id}"
+        >{teamMember.user_profiles.name}</a
+      >
+    </li>
   {/each}
 </ul>
 
@@ -32,7 +55,8 @@
 <ul>
   {#each data.team.participants as participation}
     <li>
-      <a href="/events/{participation.events.id}">{participation.events.name}</a>
+      <a href="/events/{participation.events.id}">{participation.events.name}</a
+      >
     </li>
   {/each}
 </ul>
