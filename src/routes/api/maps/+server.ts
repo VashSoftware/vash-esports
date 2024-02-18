957069088b7a3ade4640e4b5386d9d2d6bec69e0
Stan Runge <stan@stanrunge.dev>
2024-02-18 23:39:41 +0100

    fix: add osu map url parsing

--- src/routes/api/maps/+server.ts
+++ src/routes/api/maps/+server.ts
@@ -5,7 +5,16 @@ import { OSU_CLIENT_ID, OSU_CLIENT_SECRET } from "$env/static/private";
 export const POST: RequestHandler = async ({ request }) => {
   await auth.login(Number(OSU_CLIENT_ID), OSU_CLIENT_SECRET, ["public"]);
 
-  const { id } = await request.json();
+  let { id } = await request.json();
+
+  if (!id) {
+    return new Response("Invalid request", { status: 400 });
+  }
+
+  if (id.startsWith("https://osu.ppy.sh/beatmapsets/")) {
+    const urlParts = id.split("/");
+    id = urlParts[urlParts.length - 1];
+  }
 
   const map = await v2.beatmap.id.details(id);
 
