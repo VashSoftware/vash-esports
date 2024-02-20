5db675314fd983f88a0595cac06aea815e379282
Stan Runge <stan@stanrunge.dev>
2024-02-20 15:48:23 +0100

    feat: add delete map pool map button

--- src/routes/map-pools/[map_pool_id]/+page.server.ts
+++ src/routes/map-pools/[map_pool_id]/+page.server.ts
@@ -31,6 +31,8 @@ export const actions = {
     const name = formData.get("name");
     const description = formData.get("description");
 
+    // console.log(formData);
+
     const mapPool = await locals.supabase
       .from("map_pools")
       .upsert({
@@ -40,6 +42,43 @@ export const actions = {
       })
       .select("*");
 
-    console.log(mapPool);
+    const mapPoolMods = await locals.supabase
+      .from("map_pool_mods")
+      .select("*, map_pool_maps(*)")
+      .eq("map_pool_id", params.map_pool_id);
+
+    mapPoolMods.data.forEach(async (mapPoolMod) => {
+      const formDataKey = `map-pool-mods-${mapPoolMod.mod_id}`;
+
+      if (formData.get(formDataKey) == mapPoolMod.map_pool_maps.length) {
+        return;
+      }
+
+      if (formData.get(formDataKey) == 0) {
+        await locals.supabase
+          .from("map_pool_maps")
+          .delete()
+          .eq("id", mapPoolMod.id);
+      }
+
+      if (formData.get(formDataKey) > 0) {
+        await locals.supabase
+          .from("map_pool_mods")
+          .upsert({
+            id: mapPoolMod.id,
+            count: formData.get(formDataKey),
+          })
+          .select("*");
+      }
+    });
+  },
+  deleteMapPoolMap: async ({ locals, params, request }) => {
+    const formData = await request.formData();
+    const mapPoolMapId = formData.get("map-pool-map-id");
+
+    const mapPoolMap = await locals.supabase
+      .from("map_pool_maps")
+      .delete()
+      .eq("id", mapPoolMapId);
   },
 } satisfies Actions;
