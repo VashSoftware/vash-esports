fd243f14c563a4e796ac5f8ae0b6e80449d334bc
Stan Runge <stan@stanrunge.dev>
2024-06-15 17:26:48 +0200

    feat: Add conditional rendering for event-specific information in match page

--- src/routes/matches/[match_id]/+page.svelte
+++ src/routes/matches/[match_id]/+page.svelte
@@ -64,19 +64,23 @@
 <div class="py-5">
   <div>
     <div class="row align-items-center">
-      <div class="col"></div>
-      <div class="col">
-        <div class="text-center">
-          <a href="/events/{data.match.rounds.events.id}">
-            <h2>
-              {#if data.match.rounds.events.event_groups}{data.match.rounds
-                  .events.event_groups.name}
-              {/if}{data.match.rounds.events.name}
-            </h2></a
-          >
-          <h3>Grand Finals</h3>
+      {#if data.match.type == "event"}
+        <div class="col"></div>
+        <div class="col">
+          <div class="text-center">
+            <a href="/events/{data.match.rounds.events.id}">
+              <h2>
+                {#if data.match.rounds.events.event_groups}{data.match.rounds
+                    .events.event_groups.name}
+                {/if}
+
+                {data.match.rounds.events.name}
+              </h2></a
+            >
+            <h3>Grand Finals</h3>
+          </div>
         </div>
-      </div>
+      {/if}
       <div class="col d-flex justify-content-end gap-3">
         {#if data.match.ongoing}<a
             href="/matches/{data.match.id}/play"
