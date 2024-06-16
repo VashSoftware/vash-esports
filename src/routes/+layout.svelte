8bca5ec4e7149532340d17b00ae4baa91d39eec5
Stan Runge <stan@stanrunge.dev>
2024-06-16 02:42:03 +0200

    style: gap social links more

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -253,7 +253,7 @@
           <div>Made by <a href="https://vash.software">Vash Software</a></div>
         </div>
         <div
-          class="col-12 col-md d-flex justify-content-center justify-content-md-end align-items-center gap-3"
+          class="col-12 col-md d-flex justify-content-center justify-content-md-end align-items-center gap-4"
         >
           <a href="https://discord.gg/n3mZgWk">
             <svg
