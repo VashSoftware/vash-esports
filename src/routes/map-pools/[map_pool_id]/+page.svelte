345fd4897b5d5dfe4adf7447489f6a03dc8fda67
Stan Runge <stan@stanrunge.dev>
2024-07-15 06:01:44 +0200

    Add Svelte Table to map pool page

--- src/routes/map-pools/[map_pool_id]/+page.svelte
+++ src/routes/map-pools/[map_pool_id]/+page.svelte
@@ -1,7 +1,105 @@
 <script lang="ts">
   import { enhance } from "$app/forms";
+  import {
+    createColumnHelper,
+    createSvelteTable,
+    flexRender,
+    getCoreRowModel,
+    getGroupedRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    renderComponent,
+    type ColumnDef,
+  } from "@tanstack/svelte-table";
+  import { writable } from "svelte/store";
 
   export let data;
+
+  type MapPoolMap = {
+    id: number;
+    map_pool_id: number;
+    map_id: number;
+    mod_priority: number;
+    map_pool_map_mods: { code: string }[];
+    maps: {
+      mapsets: {
+        artist: string;
+        title: string;
+      };
+    };
+  };
+
+  const defaultColumns: ColumnDef<MapPoolMap>[] = [
+    {
+      accessorFn: (row) =>
+        `${row.map_pool_map_mods.map((mod) => (mod.code != null ? mod.code : "NM"))}${row.mod_priority}`,
+      header: "Mod ID",
+      cell: (info) => info.getValue(),
+    },
+    {
+      accessorFn: (row) =>
+        `${row.maps.mapsets.artist} - ${row.maps.mapsets.title}`,
+      header: "Artitst - Title [Difficulty]",
+      cell: (info) => info.getValue(),
+    },
+    {
+      accessorKey: "cs",
+      header: "Star Rating",
+      cell: (info) => info.getValue(),
+    },
+    {
+      accessorFn: (row) => "cs",
+      header: "Length",
+      cell: (info) => info.getValue(),
+    },
+    {
+      accessorKey: "cs",
+      header: "BPM",
+      cell: (info) => info.getValue(),
+    },
+    {
+      accessorKey: "cs",
+      header: "Circle Size",
+      cell: (info) => info.getValue(),
+    },
+    {
+      accessorKey: "cs",
+      header: "Overall Difficulty",
+      cell: (info) => info.getValue(),
+    },
+    {
+      accessorKey: "cs",
+      header: "Approach Rate",
+      cell: (info) => info.getValue(),
+    },
+    {
+      accessorKey: "notes",
+      header: "Notes",
+      cell: (info) => info.getValue(),
+    },
+    {
+      accessorKey: "notes",
+      header: "Actions",
+      cell: (info) => info.getValue(),
+    },
+  ];
+
+  const options = writable({
+    columns: defaultColumns,
+    data: data.mapPool.map_pool_maps,
+    getCoreRowModel: getCoreRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+
+    state: {
+      pagination: {
+        pageSize: 15,
+        pageIndex: 0,
+      },
+    },
+  });
+
+  const table = createSvelteTable(options);
 
   async function getMap(id) {
     const response = await fetch(`/api/maps`, {
@@ -48,219 +146,76 @@
 
 <div class="mb-4 d-flex align-items-center justify-content-between">
   <div>
-    <h1>{data.mapPool?.name || `Map Pool ${data.mapPool?.id}`}</h1>
-    <p>{data.mapPool?.description}</p>
+    <h1>{data.mapPool.name || `Map Pool ${data.mapPool.id}`}</h1>
+    <p>{data.mapPool.description || ""}</p>
   </div>
   <div>
     <button
       class="btn btn-primary"
       data-bs-toggle="modal"
-      data-bs-target="#editMapPoolModal">Edit Map Pool</button
+      data-bs-target="#editMapPoolModal">Settings</button
     >
   </div>
 </div>
 
-{#each data.mapPool?.map_pool_mods as mod, mod_i}
-  {#if mod.map_pool_maps.length > 0}
-    <div class="row align-items-center">
-      <div class="col-2"><h2>{mod.name}</h2></div>
-      <div class="col">
-        <div class="table-responsive">
-          <table class="table">
-            <thead>
-              <tr>
-                <th>Pool ID</th>
-                <th>osu! ID</th>
-                <th>Banner</th>
-                <th>Artist - Title [Difficulty]</th>
-                <th>Mapper</th>
-                <th>Star Rating</th>
-                <th>BPM</th>
-                <th>Time</th>
-                <th>CS</th>
-                <th>Notes</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {#each mod.map_pool_maps as map, map_i}
-                <tr class="">
-                  <td>{mod.code}{map.mod_priority}</td>
-                  <td>
-                    <form on:submit|preventDefault={debounce}>
-                      <input
-                        type="hidden"
-                        name="map-pool-mod-i"
-                        value={mod_i}
-                      />
-                      <input
-                        type="hidden"
-                        name="map-pool-map-i"
-                        value={map_i}
-                      />
+<table class="table-striped table table-hover">
+  <thead>
+    {#each $table.getHeaderGroups() as headerGroup}
+      <tr>
+        {#each headerGroup.headers as header}
+          <th scope="col" colSpan={header.colSpan}>
+            <button
+              class="btn btn-dark"
+              on:click={header.column.getToggleSortingHandler()}
+            >
+              <svelte:component
+                this={flexRender(
+                  header.column.columnDef.header,
+                  header.getContext()
+                )}
+              /></button
+            >
+          </th>
+        {/each}
+      </tr>
+    {/each}
+  </thead>
+  <tbody>
+    {#each $table.getRowModel().rows as row}
+      <tr>
+        {#each row.getVisibleCells() as cell}
+          <td>
+            <svelte:component
+              this={flexRender(cell.column.columnDef.cell, cell.getContext())}
+            />
+          </td>
+        {/each}
+      </tr>
+    {/each}
+  </tbody>
+</table>
 
-                      <input
-                        type="text"
-                        class="form-control"
-                        placeholder="Enter osu! map ID / link"
-                        name="osu-id"
-                        value={map.maps?.osu_id || ""}
-                        on:input={(event) =>
-                          event.target.form.dispatchEvent(new Event("submit"))}
-                      />
-                    </form>
-
-                    {#if map.found_maps}
-                      {#each map.found_maps as foundMap}
-                        <form action="?/addMap" method="post">
-                          <input
-                            type="hidden"
-                            name="mapId"
-                            value={foundMap.id}
-                          />
-                          <input
-                            type="hidden"
-                            name="mapPoolMapId"
-                            value={map.id}
-                          />
-
-                          <div class="card py-3">
-                            <img
-                              src="https://assets.ppy.sh/beatmaps/{foundMap
-                                ?.mapsets.osu_id}/covers/cover.jpg"
-                              class="card-img-top"
-                              alt="Map Banner"
-                            />
-                            <div class="card-body">
-                              <h5 class="card-title">
-                                {foundMap.mapsets.artist} - {foundMap.mapsets
-                                  .title}
-                              </h5>
-                              <p class="card-text">
-                                Mapped by {foundMap.mapsets.creator}
-                              </p>
-                              <div class="d-flex gap-3">
-                                <textarea
-                                  name="notes"
-                                  class="form-control"
-                                  placeholder="Notes"
-                                />
-
-                                <button type="submit" class="btn btn-primary"
-                                  >Add to Map Pool</button
-                                >
-                              </div>
-                            </div>
-                          </div>
-                        </form>
-                      {/each}
-                    {/if}
-                  </td>
-                  <td
-                    >{#if map.maps}<img
-                        src="https://assets.ppy.sh/beatmaps/{map.maps?.mapsets
-                          .osu_id}/covers/cover@2x.jpg"
-                        height="32"
-                        alt="Map Banner"
-                      />{/if}</td
-                  >
-                  <td
-                    >{#if map.maps}{map.maps?.mapsets.artist} - {map.maps
-                        ?.mapsets.title} [{map.maps?.difficulty_name}]{/if}</td
-                  >
-                  <td
-                    >{#if map.maps}{map.maps?.mapsets.creator}{/if}</td
-                  >
-                  <td
-                    >{#if map.maps}{map.maps?.star_rating}{/if}</td
-                  >
-                  <td
-                    >{#if map.maps}{map.maps?.mapsets.bpm}{/if}</td
-                  >
-                  <td
-                    >{#if map.maps}{map.maps?.mapsets.time}{/if}</td
-                  >
-                  <td
-                    >{#if map.maps}{map.maps?.circle_size}{/if}</td
-                  >
-                  <td
-                    >{#if map.maps}
-                      <form action="?/updateMapPoolMapNotes" method="post">
-                        <input
-                          type="hidden"
-                          name="map-pool-map-id"
-                          value={map.id}
-                        />
-
-                        <input
-                          type="text"
-                          name="notes"
-                          value={map.notes}
-                          class="form-control"
-                          placeholder="Add notes"
-                        />
-                      </form>
-                    {/if}</td
-                  >
-                  <td
-                    >{#if map.maps}
-                      <form
-                        action="?/deleteMapPoolMap"
-                        method="post"
-                        use:enhance
-                      >
-                        <input
-                          type="hidden"
-                          name="map-pool-map-id"
-                          value={map.id}
-                        />
-                        <button type="submit" class="btn btn-danger"
-                          ><svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            width="2em"
-                            height="2em"
-                            viewBox="0 0 24 24"
-                          >
-                            <path
-                              fill="currentColor"
-                              d="M19 4h-3.5l-1-1h-5l-1 1H5v2h14M6 19a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V7H6z"
-                            />
-                          </svg>
-                        </button>
-                      </form>
-                    {/if}
-                  </td>
-                </tr>
-              {/each}
-            </tbody>
-          </table>
+<form action="?/editMapPool" method="post">
+  <div
+    class="modal fade"
+    id="editMapPoolModal"
+    tabindex="-1"
+    aria-labelledby="exampleModalLabel"
+    aria-hidden="true"
+  >
+    <div class="modal-dialog modal-dialog-centered">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="exampleModalLabel">
+            Edit {data.mapPool?.name || `Map Pool ${data.mapPool.id}`}
+          </h1>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="modal"
+            aria-label="Close"
+          ></button>
         </div>
-      </div>
-    </div>
-  {/if}
-{/each}
-
-<div
-  class="modal fade"
-  id="editMapPoolModal"
-  tabindex="-1"
-  aria-labelledby="exampleModalLabel"
-  aria-hidden="true"
->
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">
-          Edit {data.mapPool?.name || `Map Pool ${data.mapPool.id}`}
-        </h1>
-        <button
-          type="button"
-          class="btn-close"
-          data-bs-dismiss="modal"
-          aria-label="Close"
-        ></button>
-      </div>
-      <form action="?/editMapPool" method="post">
         <div class="modal-body">
           <div class="mb-3">
             <h2>Information</h2>
@@ -291,22 +246,6 @@
             >
 
             <h2>Maps</h2>
-            <div class="row">
-              {#each data.mapPool.map_pool_mods as mod}
-                <div class="col col-3">
-                  <label for="mapPoolMod" class="form-label">{mod.code}</label>
-                  <input
-                    type="number"
-                    class="form-control"
-                    id="mapPoolMaps"
-                    name="map-pool-mods-{mod.id}"
-                    value={mod.map_pool_maps.length}
-                    min="1"
-                    max="99"
-                  />
-                </div>
-              {/each}
-            </div>
           </div>
 
           <div class="mb-3 d-flex justify-content-between align-items-center">
@@ -325,7 +264,7 @@
           >
           <button type="submit" class="btn btn-primary">Save changes</button>
         </div>
-      </form>
+      </div>
     </div>
   </div>
-</div>
+</form>
