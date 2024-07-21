29808a6a4528bad88403ad29ec1c624b0067c75e
Stan Runge <stan@stanrunge.dev>
2024-07-21 20:00:49 +0200

    Fixes Add mod ID editing #281

--- src/routes/map-pools/[map_pool_id]/ModIdSelectors.svelte
+++ src/routes/map-pools/[map_pool_id]/ModIdSelectors.svelte
@@ -1,12 +1,43 @@
 <script>
   import { enhance } from "$app/forms";
-
-  export let mapPoolMapId;
+  export let mods;
+  export let mapPoolMap;
 </script>
 
-<form action="?/removeMapPoolMap" method="post">
-  <input type="hidden" name="map-pool-map-id" value={mapPoolMapId} />
-  <div class="text-center">
-    <button type="submit" class="btn btn-danger">-</button>
-  </div>
-</form>
+<div class="d-flex gap-1 text-center">
+  <form
+    action="?/editMod"
+    method="post"
+    on:change={(e) => {
+      e.currentTarget.requestSubmit();
+    }}
+  >
+    <input type="hidden" name="map-pool-map-id" value={mapPoolMap.id} />
+    <select name="mod-id" class="form-select me-4">
+      {#each mods as mod}
+        <option
+          value={mod.id}
+          selected={mod.id == mapPoolMap.map_pool_map_mods[0]?.mods.id}
+        >
+          {mod.code || "NM"}
+        </option>
+      {/each}
+    </select>
+  </form>
+
+  <form
+    action="?/editModPriority"
+    method="post"
+    on:change={(e) => {
+      e.currentTarget.requestSubmit();
+    }}
+  >
+    <input type="hidden" name="map-pool-map-id" value={mapPoolMap.id} />
+    <input
+      type="number"
+      name="mod-priority"
+      class="form-control"
+      value={mapPoolMap.mod_priority}
+    />
+  </form>
+</div>
