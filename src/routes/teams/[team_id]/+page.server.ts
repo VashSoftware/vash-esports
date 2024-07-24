2a4ece5b9f2854987adef0c7b11ed8580abcf0a8
Stan Runge <stan@stanrunge.dev>
2024-07-24 18:56:01 +0200

    Fixes Personal team doesn't redirect to user page #349

--- src/routes/teams/[team_id]/+page.server.ts
+++ src/routes/teams/[team_id]/+page.server.ts
@@ -1,13 +1,18 @@
+import { redirect } from "@sveltejs/kit";
 import type { Actions, PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ params, locals }) => {
   const { data, error } = await locals.supabase
     .from("teams")
     .select(
-      `*, team_members(*, user_profiles(*)), participants(events(id, name, event_groups(*)))`,
+      `*, team_members(*, user_profiles(*)), participants(events(id, name, event_groups(*)))`
     )
     .eq("id", params.team_id)
     .single();
+
+  if (data.is_personal_team) {
+    throw redirect(303, `/users/${data.team_members[0].user_profiles.id}`);
+  }
 
   return {
     team: data,
