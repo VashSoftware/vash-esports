add61a6e87cb5d4f802c9028183945079458ccc7
Stan Runge <stan@stanrunge.dev>
2024-09-16 17:50:20 +0200

    Remove supabase

--- packages/frontend/web/src/routes/+layout.ts
+++ packages/frontend/web/src/routes/+layout.ts
@@ -3,39 +3,9 @@ import {
   PUBLIC_SUPABASE_URL,
 } from "$env/static/public";
 import type { LayoutLoad } from "./$types";
-import {
-  createBrowserClient,
-  createServerClient,
-  isBrowser,
-  parse,
-} from "@supabase/ssr";
-import { dev } from "$app/environment";
 
 export const load: LayoutLoad = async ({ fetch, data, depends }) => {
-  depends("supabase:auth");
 
-  const supabase = isBrowser()
-    ? createBrowserClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
-        global: { fetch },
-        cookies: {
-          get(key) {
-            const cookie = parse(document.cookie);
-            return cookie[key];
-          },
-        },
-      })
-    : createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
-        global: { fetch },
-        cookies: {
-          get() {
-            return JSON.stringify(data.session);
-          },
-        },
-      });
-
-  const session = isBrowser()
-    ? (await supabase.auth.getSession()).data.session
-    : data.session;
 
   const eventsPromise = supabase.from("events").select("*");
   const matchesPromise = supabase.from("matches").select("*");
